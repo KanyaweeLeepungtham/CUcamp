d9682b86c9dbe79ead02f88cb6d32a7ddc0cc541
admin <admin@admins-MacBook-Pro.local>
2024-06-22 21:17:55 -0700

    add Q&A

--- Bot chat/Bot chat/Preview Content/Bot response.swift
+++ Bot chat/Bot chat/Preview Content/Bot response.swift
@@ -16,6 +16,12 @@ func getBotResponse(message: String) -> String {
         return "Talk to you later!"
     } else if tempMessage.contains("how are you") {
         return "I'm fine, how about you?"
+    } else if tempMessage.contains("how old are you") {
+        return "I'm xxxx year old"
+    } else if tempMessage.contains("weather") {
+        return " I guess it will sunnt all day!!!"
+    } else if tempMessage.contains("what are you doing") {
+        return " I'm talking with you!!"
     } else {
         return "That's cool."
     } 
