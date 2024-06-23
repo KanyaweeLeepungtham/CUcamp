dd7e87d96b96582a56f356b2253b8dc1f9aaba95
admin <admin@admins-MacBook-Pro.local>
2024-06-22 20:05:20 -0700

    CU camp

---
+++ demo github/demo github/demo_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_githubApp.swift
+//  demo github
+//
+//  Created by admin on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct demo_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
